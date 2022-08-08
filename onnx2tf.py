63ed89f1f2e3b0fcfd4bf500fe78717c241224f8
TUAN-ANH <72487483+TUANANH-INU@users.noreply.github.com>
2022-08-08 14:28:52 +0700

    Update onnx2tf.py

--- onnx2tf.py
+++ onnx2tf.py
@@ -10,3 +10,19 @@ tf_model_path = 'weights_tf'
 onnx_model = onnx.load(onnx_model_path)
 tf_rep = prepare(onnx_model)
 tf_rep.export_graph(tf_model_path)
+
+# ------------------- check weight tf -------------------- #
+# import library
+import tensorflow as tf
+
+# set parameter
+tf_model_path = 'weights_tf'
+
+# load weight tensorflow
+model = tf.saved_model.load(tf_model_path)
+model.trainable = False
+
+# create input and check model
+input_tensor = tf.random.uniform([1, 512, 512, 3])
+out = model(**{'input': input_tensor})
+print(out)
